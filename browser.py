dab968cd775cc81bd5126f26f9dd6d3db974a1e4
HariniYadav <67147998+HariniYadav@users.noreply.github.com>
2020-07-27 13:49:14 +0530

    Delete browser.py

--- browser.py
+++
@@ -1,7 +0,0 @@
-from selenium import webdriver
-
-driver=webdriver.Chrome(executable_path=r"C:\chromedriver.exe")
-driver.maximize_window()
-driver.get("https://admin-demo.nopcommerce.com/admin/")
-print(driver.title)
-driver.close()
